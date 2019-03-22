cd511dc4b6a2fef2623db4cfd57b1bda803d2413
rapabo <48825817+rapabo@users.noreply.github.com>
2019-03-22 16:05:34 +0100

    Initial commit

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+import Layout from "../layouts/default";
+
+export default () => (
+  <Layout>
+    <h1>Phone</h1>
+    <p>Call me.</p>
+    <h1>E-Mail</h1>
+    <p>Drop me a line</p>
+  </Layout>
+);
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Layout from "../layouts/default";
+
+export default () => (
+  <Layout>
+    <h1>Rafael Boog</h1>
+    <p>I convey complex messages.</p>
+    <h2>CV</h2>
+    <p>
+      Got time? Let me tell you a story about a thing or two. Click this Button
+    </p>
+    <p>
+      Kanye West said: > We're living the future so > the present is our past.
+    </p>
+    <h3>Non work related accomplishments</h3>
+    <p>Balancing two eggs with one spoon</p>
+  </Layout>
+);
